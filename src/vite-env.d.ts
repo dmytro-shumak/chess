cfd4e8f56111c415a66161b7562a844128ba5b56
Dmytro <dima06444@gmail.com>
2026-04-20 11:12:45 +0200

    feat: add socket.io-client dependency and update .gitignore to include .env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_ONLINE_SOCKET_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.png" {
   const src: string;
   export default src;
